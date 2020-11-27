9cfc0b6a1f37f553883a6f8541774dc0a43a7a7d
shin <huutrimega@gmail.com>
2020-11-27 18:55:46 +0700

    push heroku

--- src/Cryptos/DiffieHellman.js
+++ src/Cryptos/DiffieHellman.js
@@ -7,26 +7,31 @@ class DiffieHellman {
 	}
 
 	getPrime() {
+		//chuyên thành base64
 		const prime = this.server.getPrime().toString("base64");
 		return prime;
 	}
 
 	getGenerator() {
+		//chuyên thành base64
 		const generator = this.server.getGenerator().toString("base64");
 		return generator;
 	}
 
 	getPublicKey() {
+		//chuyên thành base64
 		const publicKey = this.server.getPublicKey().toString("base64");
 		return publicKey;
 	}
 
 	getPrivateKey() {
+		//chuyên thành basee64
 		const privateKey = this.server.getPrivateKey().toString("base64");
 		return privateKey;
 	}
 
 	generateSecretKey(publicKey) {
+		//chuyển base thành buffer
 		const buffer = Buffer.from(publicKey, "base64");
 		return this.server.computeSecret(buffer).toString("base64");
 	}
